8b198b63f5f0b91897773d8c6e77c086124ed719
CaptainKryuk <35116092+CaptainKryuk@users.noreply.github.com>
2018-06-12 14:10:35 +0300

    Add auth login and impove views

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url, include
 from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    path('', views.snippet_list, name='snippets_list'),
-    url(r'^(?P<pk>[0-9]+)/$', views.snippet_detail, name='snippet_detail')
+    path('snippets/', views.SnippetList.as_view(), name='snippets_list'),
+    url(r'snippets/^(?P<pk>[0-9]+)/$', views.SnippetList.as_view(), name='snippet_detail'),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9])$', views.UserDetail.as_view()),
+    url(r'^api-auth', include('rest_framework.urls')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -1,54 +1,73 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from django.http import Http404
+from snippets.models import Snippet
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+from rest_framework import generics         # Using for generic class-based views
+from rest_framework import permissions
 
-
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
+class SnippetList(APIView):
     """
     List all code snippets, or create a new snippet
     :param request:
     :return:
     """
-    if request.method == 'GET':
+    def get(self, requesr, format=None):
         snippets = Snippet.objects.all()
-        serializers = SnippetSerializer(snippets, many=True)
-        return Response(serializers.data)
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid:
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
+class SnippetDetail(APIView):
     """
     Retrieve, update or delete a code snippet
     :param request:
     :param pk:
     :return:
     """
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
